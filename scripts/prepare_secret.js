13138b4c707beaa021197c99a4612d1c08e12c3b
ghostbot <jb@jbson.net>
2023-04-01 00:24:37 +0200

    update prepare script

--- scripts/prepare_secret.js
+++ scripts/prepare_secret.js
@@ -24,6 +24,7 @@ function convertStringToBytes(input) {
 
 task("prepare-secret", "Prepare a secret for reveal")
     .addParam("source", "The source of the secret")
+    .addOptionalParam("valuesize", "The size of each value to be revealed", 2, types.int)
     .addOptionalParam('key', 'The key to cipher the secret')
     .addOptionalParam('iv', 'The iv to cipher the secret')
     .setAction(async (taskArgs, hre) => {
@@ -62,23 +63,28 @@ task("prepare-secret", "Prepare a secret for reveal")
         // deploy the contract
         const instance = await contract.deploy();
         // set the data to be ciphered
-        await instance.setHiddenValues(secret);
+        await instance.setHiddenValues(secret, taskArgs.valuesize);
         //  set key and iv
         await instance.setRevealKey(key, iv);
         // prepare the secret
+        await instance.reveal();
         const cipherData = [];
-        const decipherData = [];
         for (let i = 0; i < secret.length; i++) {
-            const cipher = await instance.revealIndex(i);
+            const cipher = await instance.getHiddenValue(i, taskArgs.valuesize);
             // big number to number
             cipherData.push(cipher.toNumber());
             //cipherData.push(cipher.toHexString());
         }
+        
         // set the data to be deciphered
-        await instance.setHiddenValues(cipherData);
+        const decipherData = [];
+        await instance.setHiddenValues(cipherData, taskArgs.valuesize);
+        await instance.setRevealKey(key, iv);
+        // prepare the secret
+        await instance.reveal();
         // decipher the data to check if it matches the original data
         for (let i = 0; i < cipherData.length; i++) {
-            decipherData.push(await instance.revealIndex(i));
+            decipherData.push(await instance.getHiddenValue(i, taskArgs.valuesize));
             assert(decipherData[i] == secret[i], `Deciphered data ${decipherData[i]} does not match the original data ${secret[i]}`);
         }
         console.log(`${chalk.yellow("Ciphered secret:")}\n${cipherData}`);
@@ -104,7 +110,8 @@ task("prepare-secret", "Prepare a secret for reveal")
             key_to_use: key,
             iv_to_use: iv,
             ciphered_secret_to_use: cipherData,
-            secret_to_cipher: file.secret
+            secret_to_cipher: file.secret,
+            hidden_value_bytes_size: taskArgs.valuesize,
         }));
         // display the file path 
         console.log(`\n${chalk.yellow('The secret data has been written to')} ${path.join(__dirname, "output")}\n${chalk.blue('keep it safe!')}`);
